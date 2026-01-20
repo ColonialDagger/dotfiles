4eec11be78282a46d7be581c8d98ee92438346d0
Daniel Garcia <dannyg305@hotmail.com>
2026-01-20 01:11:33 -0500

    Update scripts/lxc_setup.sh

--- scripts/executable_lxc_setup.sh
+++ scripts/executable_lxc_setup.sh
@@ -18,7 +18,7 @@ prompt() {
 
 # Define your package list
 extra_packages=(
-    btop htop sudo nvtop fzf ncdu curl vim magic-wormhole cron nala tldr
+    btop htop sudo nvtop fzf ncdu curl vim magic-wormhole cron nala tldr net-tools
 )
 
 # PROMPTS
