c16dddbadf19d0fba788ac80ea3fdfe1b9559eae
Daniel Garcia <dannyg305@hotmail.com>
2026-04-14 05:24:41 -0400

    Update scripts/ksa_updater.sh

--- scripts/executable_ksa_updater.sh
+++ scripts/executable_ksa_updater.sh
@@ -2,24 +2,22 @@
 
 # Get versions
 upstream_version=$(curl -fsSL https://ksa-linux.ahwoo.com/ | grep class=\"filename\" | grep -oP 'v\K[0-9.]+(?=\.tar\.gz)')
-aur_version=$(curl -fsSL https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=kittenspaceagency-bin | grep -oP '(?<=pkgver=)[0-9.]+')
-echo $upstream_version
-echo $aur_version
+aur_version=$(curl -fsSL https://aur.archlinux.org/packages/kittenspaceagency-bin | grep "Package Details:" | grep -oP '(?<=kittenspaceagency-bin )[0-9.]+')
 
 if [[ "$aur_version" != "$upstream_version" ]]; then
 
-	cd /tmp
-	git clone ssh://aur@aur.archlinux.org/kittenspaceagency-bin.git
-	cd kittenspaceagency-bin
+        cd /tmp
+        git clone ssh://aur@aur.archlinux.org/kittenspaceagency-bin.git
+        cd kittenspaceagency-bin
 
-	sed -i "s/^pkgver=.*/pkgver=${upstream_version}/" PKGBUILD
-	updpkgsums
-	makepkg --printsrcinfo > .SRCINFO
+        sed -i "s/^pkgver=.*/pkgver=${upstream_version}/" PKGBUILD
+        updpkgsums
+        makepkg --printsrcinfo > .SRCINFO
 
-	git add PKGBUILD .SRCINFO
-	git commit -m "Updated version."
-	git push
+        git add PKGBUILD .SRCINFO
+        git commit -m "Updated version."
+        git push
 
-	rm -rf /tmp/kittenspaceagency-bin
+        rm -rf /tmp/kittenspaceagency-bin
 
 fi
