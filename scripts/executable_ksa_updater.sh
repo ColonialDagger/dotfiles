f5c9c20f3aece7ffe94799c9bfaf79a26a297729
Daniel Garcia <dannyg305@hotmail.com>
2026-04-14 05:18:00 -0400

    Update scripts/ksa_updater.sh

--- scripts/executable_ksa_updater.sh
+++ scripts/executable_ksa_updater.sh
@@ -3,6 +3,8 @@
 # Get versions
 upstream_version=$(curl -fsSL https://ksa-linux.ahwoo.com/ | grep class=\"filename\" | grep -oP 'v\K[0-9.]+(?=\.tar\.gz)')
 aur_version=$(curl -fsSL https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=kittenspaceagency-bin | grep -oP '(?<=pkgver=)[0-9.]+')
+echo $upstream_version
+echo $aur_version
 
 if [[ "$aur_version" != "$upstream_version" ]]; then
 
