9cb400d2dd5543024025d13ea23a1f8ec9f0a1b7
Daniel Garcia <dannyg305@hotmail.com>
2026-04-14 05:13:36 -0400

    Add scripts/ksa_updater.sh

---
+++ scripts/executable_ksa_updater.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Get versions
+upstream_version=$(curl https://ksa-linux.ahwoo.com/ | grep class=\"filename\" | grep -oP 'v\K[0-9.]+(?=\.tar\.gz)')
+aur_version=$(curl https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=kittenspaceagency-bin | grep -oP '(?<=pkgver=)[0-9.]+')
+
+# Compare versions
+if [[ "$aur_version" != "$upstream_version" ]]; then
+
+	cd /tmp
+	git clone ssh://aur@aur.archlinux.org/kittenspaceagency-bin.git
+	cd kittenspaceagency-bin
+
+	sed -i "s/^pkgver=.*/pkgver=${upstream_version}/" PKGBUILD
+	updpkgsums
+	makepkg --printsrcinfo > .SRCINFO
+
+	git add PKGBUILD .SRCINFO
+	git commit -m "Updated version."
+	git push
+
+	rm -rf /tmp/kittenspaceagency-bin
+
+fi
