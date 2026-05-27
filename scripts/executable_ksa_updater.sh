44c4eac31088ed4bc3b9415e6a96ecc07a847e18
Daniel Garcia <dannyg305@hotmail.com>
2026-05-27 00:32:05 -0400

    Update scripts/ksa_updater.sh

--- scripts/executable_ksa_updater.sh
+++ scripts/executable_ksa_updater.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 # Get versions
-upstream_version=$(curl -fsSL https://ksa-archive.net/assets/index-C4qpkk_n.js | grep -oP 'setup_ksa_v\K[0-9.]+(?=\.tar\.gz)' | tac | head -n 1)
+jsfile=$(curl -fsSL https://ksa-archive.net \
+  | grep -oP 'src="/assets/\Kindex-[^"]+')
+upstream_version=$(curl -fsSL "https://ksa-archive.net/assets/$jsfile" \
+  | grep -oP 'setup_ksa_v\K[0-9.]+(?=\.tar\.gz)' \
+  | tac \
+  | head -n 1)
 aur_version=$(curl -fsSL https://aur.archlinux.org/packages/kittenspaceagency-bin | grep "Package Details:" | grep -oP '(?<=kittenspaceagency-bin )[0-9.]+')
 
 if [[ "$aur_version" != "$upstream_version" ]]; then
+	
+	echo "New version detected!"
+	echo "Upstream: $upstream_version"
+	echo "AUR:      $aur_version"
+	echo
 
         cd /tmp
         git clone ssh://aur@aur.archlinux.org/kittenspaceagency-bin.git
